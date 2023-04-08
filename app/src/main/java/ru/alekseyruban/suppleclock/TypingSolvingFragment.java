b0cb197585aee69dee65dccd8e2f8e8c4de9a365
aleksey-ruban <alekseyruban555@gmail.com>
2023-04-08 19:31:25 +0300

    TypingSolving Fragment

---
+++ app/src/main/java/ru/alekseyruban/suppleclock/TypingSolvingFragment.java
@@ -0,0 +1,36 @@
+package ru.alekseyruban.suppleclock;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TypingSolvingFragment extends Fragment {
+
+    private TypingSolvingViewModel mViewModel;
+
+    public static TypingSolvingFragment newInstance() {
+        return new TypingSolvingFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_typing_solving, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(TypingSolvingViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ru/alekseyruban/suppleclock/TypingSolvingViewModel.java
@@ -0,0 +1,7 @@
+package ru.alekseyruban.suppleclock;
+
+import androidx.lifecycle.ViewModel;
+
+public class TypingSolvingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
