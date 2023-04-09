3a8bc84629db63998cc43a777dba29b323bb3903
aleksey-ruban <alekseyruban555@gmail.com>
2023-04-09 15:17:18 +0300

    ShakingSolvingFragment

---
+++ app/src/main/java/ru/alekseyruban/suppleclock/ShakingSolvingFragment.java
@@ -0,0 +1,36 @@
+package ru.alekseyruban.suppleclock;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ShakingSolvingFragment extends Fragment {
+
+    private ShakingSolvingViewModel mViewModel;
+
+    public static ShakingSolvingFragment newInstance() {
+        return new ShakingSolvingFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_shaking_solving, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ShakingSolvingViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ru/alekseyruban/suppleclock/ShakingSolvingViewModel.java
@@ -0,0 +1,7 @@
+package ru.alekseyruban.suppleclock;
+
+import androidx.lifecycle.ViewModel;
+
+public class ShakingSolvingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
